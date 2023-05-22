088bc7f6640bb9636d3fce7cfbe0b3a7a29550e7
Misha Voloshyniuk <77201178+h1fired@users.noreply.github.com>
2023-05-22 21:56:17 +0300

    Add Users API
    * Add Users API
    
    * Add Tests to Users API
    
    * Add Tests to Users API v2

---
+++ chatbot/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/', views.users_list, name='api_users_list'),
+    path('users/create/', views.user_create_or_get, name='api_user_create'),
+    path('users/<int:social_id>/', views.user_detail, name='api_user'),
+    path('users/update/<int:social_id>/', views.user_update, name='api_user_update'),
+]
--- chatbot/users/models.py
+++ chatbot/users/models.py
@@ -3,14 +3,13 @@ from django.utils import timezone
 
 class UserProfile(models.Model):
     social_id = models.PositiveIntegerField(verbose_name='ID користувача в телеграмі')
-    username = models.CharField(max_length=128, verbose_name='Ім\'я користувача')
+    username = models.CharField(max_length=128, unique=True, verbose_name='Нік користувача')
+    first_name = models.CharField(max_length=128, blank=True, null=True, verbose_name='Ім\'я користувача')
+    last_name = models.CharField(max_length=128, blank=True, null=True, verbose_name='Прізвище користувача')
     specialty = models.CharField(max_length=128, blank=True, null=True, verbose_name='Спеціальність студента')
     course_num = models.PositiveIntegerField(blank=True, null=True, verbose_name='Курс студента')
     group = models.CharField(blank=True, null=True, max_length=16, verbose_name='Група студента')
     create_date = models.DateTimeField(default=timezone.now, verbose_name='Дата створення профілю', editable=False)
-    
-    class Meta:
-        verbose_name = 'Профіль'
         
     def __str__(self):
         return self.username
