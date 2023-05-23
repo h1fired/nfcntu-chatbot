d6013b95024cd7e9599f85937e28004f9408ccf8
h1fired <misha01213@gmail.com>
2023-05-23 18:04:09 +0300

    Safe API Update - Add Object DoesNotExist Exception Condition

--- chatbot/api/views.py
+++ chatbot/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.models import UserProfile
 from .serializers import UserProfileSerializator
-from rest_framework import status
+from rest_framework import status 
 from .permissions import APIKeyPermission
 
 @api_view(['GET'])
@@ -17,9 +17,12 @@ def users_list(request):
 @api_view(['GET'])
 @permission_classes([APIKeyPermission])
 def user_detail(request, social_id):
-    user = UserProfile.objects.get(social_id=social_id)
-    serialized_user = UserProfileSerializator(user, many=False)
-    return Response(serialized_user.data)
+    try:
+        user = UserProfile.objects.get(social_id=social_id)
+        serialized_user = UserProfileSerializator(user, many=False)
+        return Response(serialized_user.data)
+    except UserProfile.DoesNotExist:
+        return HttpResponseNotFound(f'User with social_id={social_id} not found')
 
 @api_view(['POST'])
 @permission_classes([APIKeyPermission])
@@ -40,7 +43,7 @@ def user_create_or_get(request):
 @permission_classes([APIKeyPermission])
 def user_update(request, social_id):
     if UserProfile.objects.filter(social_id=social_id).exists() is not True:
-        return HttpResponseNotFound()
+        return HttpResponseNotFound(f'User with social_id={social_id} not found')
         
     user = UserProfile.objects.get(social_id=social_id)
     serializer = UserProfileSerializator(instance=user, data=request.data, partial=True)
